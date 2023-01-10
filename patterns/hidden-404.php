5b2bf94aa6529d01328ad03884abf2c1b5d7b14d
robicse11127 <robizmailbox@gmail.com>
2023-01-10 16:07:03 +0600

    Initial theme structure added

---
+++ patterns/hidden-404.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Title: Hidden 404
+ * Slug: fse-wp-theme/hidden-404
+ * Inserter: no
+ */
+?>
+ <!-- wp:spacer {"height":"var(--wp--preset--spacing--30)"} -->
+ <div style="height:var(--wp--preset--spacing--30)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"level":1,"align":"wide"} -->
+<h1 class="alignwide">404 Error code for a webpage that is not found.</h1>
+<!-- /wp:heading -->
+
+<!-- wp:group {"align":"wide","layout":{"type":"default"},"style":{"spacing":{"margin":{"top":"5px"}}}} -->
+<div class="wp-block-group alignwide" style="margin-top:5px">
+    <!-- wp:paragraph -->
+    <p>This page could not be found.</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:search {"label":"Search here","showLabel":false,"width":100,"widthUnit":"%","buttonText":"<?php esc_attr_e( 'Search', 'twentytwentythree' ); ?>","buttonUseIcon":true,"align":"center"} /-->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"var(--wp--preset--spacing--70)"} -->
+<div style="height:var(--wp--preset--spacing--70)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
